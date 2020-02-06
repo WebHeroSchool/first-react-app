272ddf9e075d9efd6819ccd59650e05a8622fb16
ksenia <kkennny@gmail.com>
2020-02-06 17:58:19 +0300

    fromZero

--- src/App.js
+++ src/App.js
@@ -1,53 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {c, l} from "./number";
 
-const title = "Title";
-const truth = true;
+const el = (<div>Hello!</div>);
 
 
-class App extends Component {
+/*class App extends Component {
   render() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-
-                <p>count*length = {c * l}</p>
-
-                <h1 style={{color: 'tomato', margin: 0}}> {title}</h1>
-                <p style={{color: 'tomato', margin: 10}}>Это {11} урок из {43}. Осталось {43 - 11} уроков.</p>
-
-                {truth &&
-                <p style={{color: 'tomato', margin: 10}}> логические операции</p>
-                }
-                <p style={{color: 'tomato', margin: 10}}> undefined {undefined} , null {null}, true {true} и false {false} не будут
-                    выводиться в разметку</p>
-
-                <p style={{color: 'tomato', margin: 10}}> undefined {undefined} , null {null}, true {true} и false {false} не будут
-                    выводиться в разметку</p>
-
-                <div className="app__block" style={{color: 'tomato',}}>Тернарные операторы:
-                    {(truth) ?
-                        <p style={{margin: 10}}>ok</p> :
-                        <p style={{margin: 10}}>bad</p>}
-                </div>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+    return ( );
   }
-}
+}*/
 
-export default App;
+export {el};
