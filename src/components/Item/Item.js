a0131c54bea623f1c1ff2d1aa38d7d2a9d22aa54
ksenia <kkennny@gmail.com>
2020-02-13 15:16:43 +0300

    life cycle

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,27 +5,46 @@ import Checkbox from '@material-ui/core/Checkbox';
 import DeleteIcon from '@material-ui/icons/Delete';
 import PropTypes from 'prop-types';
 
-const Item = ({task, setTaskState, removeTask}) => (
-    <div className={styles.itemWrap}>
-        <Checkbox
-            value="checkedA"
-            checked={task.isDone}
-            inputProps={{ 'aria-label': 'Checkbox A' }}
-        />
-        <span onClick={() => setTaskState(task)}
-            className={
-            classnames({
-                [styles.item]: true,
-                [styles.done]: task.isDone
-            })
-        }>
+class Item extends React.Component {
+    componentWillMount() {
+        console.log('componentWillMount');
+    }
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+    }
+
+    render() {
+        const {task, setTaskState, removeTask} = this.props;
+
+        return (
+            <div className={styles.itemWrap}>
+                <Checkbox
+                    value="checkedA"
+                    checked={task.isDone}
+                    inputProps={{ 'aria-label': 'Checkbox A' }}
+                />
+                <span onClick={() => setTaskState(task)}
+                      className={
+                          classnames({
+                              [styles.item]: true,
+                              [styles.done]: task.isDone
+                          })
+                      }>
             {task.value}
         </span>
-        <DeleteIcon className={styles.icon}
-                    onClick={() => removeTask(task)}
-        />
-    </div>
-);
+                <DeleteIcon className={styles.icon}
+                            onClick={() => removeTask(task)}
+                />
+            </div>
+        );
+    }
+}
 
 Item.propTypes = {
     task: PropTypes.object,
